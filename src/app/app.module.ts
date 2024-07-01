70aed2eac4fb85cb73a4db3bbf2a5a08f11a5a46
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-07-01 20:00:37 +0500

    asbdlkja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
